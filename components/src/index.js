6305cc994430e674548246ff7936acd1c9cec1eb
chenfrank647 <chenfrank647@gmail.com>
2021-05-11 20:09:51 -0400

    Children and props

--- components/src/index.js
+++ components/src/index.js
@@ -1,8 +1,40 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import CommentDetail from './CommentDetail'
+import ApprovalCard from './ApprovalCard'
 
 const App = () => {
-    return <div>Hi there!</div>
+    return (
+        <div className="ui container comments">
+            <ApprovalCard>
+                <div><h4>Warning!</h4> Are you sure</div>
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail 
+                    author = "Sam" 
+                    content = "Nice Nice Nice" 
+                    time = "8:45"/>
+            </ApprovalCard>
+            <ApprovalCard>
+            <CommentDetail 
+                author = "Alex" 
+                content = " Hello Hello Hello" 
+                time = "7:55"/>
+            </ApprovalCard>
+            <ApprovalCard>
+            <CommentDetail 
+                author = "Jane" 
+                content = "OK" 
+                time = "9:44"/>
+            </ApprovalCard>
+            <ApprovalCard>
+            <CommentDetail 
+                author = "Sandy" 
+                content = "NO!" 
+                time = "4:45"/>
+            </ApprovalCard>
+        </div>
+    )
 };
 
 ReactDOM.render(<App />, document.querySelector('#root'));
